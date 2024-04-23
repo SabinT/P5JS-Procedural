69582c19e7a53864107d2f5b6a9cf98d091f4a0e
Sabin <sabintim@outlook.com>
2024-04-22 21:33:39 -0700

    ar related cleanup

--- belred-ar-poc/ar-easing.js
+++ belred-ar-poc/ar-easing.js
@@ -1,32 +1,31 @@
 // From https://easings.net/
 
 export function easeInOutQuart(x) {
-    return x < 0.5 ? 8 * x * x * x * x : 1 - Math.pow(-2 * x + 2, 4) / 2;
-  }
-  
-  export function easeInOutQuad(x) {
-    return x < 0.5 ? 2 * x * x : 1 - Math.pow(-2 * x + 2, 2) / 2;
-  }
-  
-  export function easeOutElastic(x) {
-    const c4 = (2 * Math.PI) / 3;
-  
-    return x === 0
-      ? 0
-      : x === 1
-      ? 1
-      : Math.pow(2, -10 * x) * Math.sin((x * 10 - 0.75) * c4) + 1;
-  }
-  
-  export function easeInOutElastic(x) {
-    const c5 = (2 * Math.PI) / 4.5;
-  
-    return x === 0
-      ? 0
-      : x === 1
-      ? 1
-      : x < 0.5
-      ? -(Math.pow(2, 20 * x - 10) * Math.sin((20 * x - 11.125) * c5)) / 2
-      : (Math.pow(2, -20 * x + 10) * Math.sin((20 * x - 11.125) * c5)) / 2 + 1;
-  }
-  
+  return x < 0.5 ? 8 * x * x * x * x : 1 - Math.pow(-2 * x + 2, 4) / 2
+}
+
+export function easeInOutQuad(x) {
+  return x < 0.5 ? 2 * x * x : 1 - Math.pow(-2 * x + 2, 2) / 2
+}
+
+export function easeOutElastic(x) {
+  const c4 = (2 * Math.PI) / 3
+
+  return x === 0
+    ? 0
+    : x === 1
+    ? 1
+    : Math.pow(2, -10 * x) * Math.sin((x * 10 - 0.75) * c4) + 1
+}
+
+export function easeInOutElastic(x) {
+  const c5 = (2 * Math.PI) / 4.5
+
+  return x === 0
+    ? 0
+    : x === 1
+    ? 1
+    : x < 0.5
+    ? -(Math.pow(2, 20 * x - 10) * Math.sin((20 * x - 11.125) * c5)) / 2
+    : (Math.pow(2, -20 * x + 10) * Math.sin((20 * x - 11.125) * c5)) / 2 + 1
+}
--- belred-ar-poc/hexgrid.js
+++ belred-ar-poc/hexgrid.js
@@ -1,9 +1,23 @@
-import { getDistOddr, hexToCartesianOddr, Polygon, vec2, PI, rotateAround } from "./ar-common.js";
-import { sqRand } from "./ar-rand.js";
-import { easeInOutQuad } from "./ar-easing.js";
-import { getAnimCompleteCount, getAnimProgress, isAnimActive, setMillis, startAnim, stepAnimate } from "./ar-anim.js";
+import {
+  getDistOddr,
+  hexToCartesianOddr,
+  Polygon,
+  vec2,
+  PI,
+  rotateAround,
+} from "./ar-common.js"
+import { sqRand } from "./ar-rand.js"
+import { easeInOutQuad } from "./ar-easing.js"
+import {
+  getAnimCompleteCount,
+  getAnimProgress,
+  isAnimActive,
+  setMillis,
+  startAnim,
+  stepAnimate,
+} from "./ar-anim.js"
 
-import { config } from "./ar-config.js";
+import { config } from "./ar-config.js"
 
 const VERTEX_SHADER = `
 varying vec3 vLocalPosition;
@@ -37,7 +51,7 @@ void main() {
 
     gl_Position = projectionMatrix * modelViewMatrix * vec4(vLocalPosition, 1.0);
 }
-`;
+`
 
 const FRAGMENT_SHADER = `
 uniform float xMin, xMax, yMin, yMax;
@@ -68,49 +82,49 @@ void main() {
         gl_FragColor = texColor;
     }
 }
-`;
+`
 
-AFRAME.registerComponent('size-inches', {
+AFRAME.registerComponent("size-inches", {
   schema: {
-    width: {type: 'number', default: 36},
-    height: {type: 'number', default: 48},
-    grayToAlpha: {type: 'boolean', default: false}
+    width: { type: "number", default: 36 },
+    height: { type: "number", default: 48 },
+    grayToAlpha: { type: "boolean", default: false },
   },
 
   init: function () {
-    const widthScale = this.data.width * 0.0254; // Convert width from inches to meters
-    const heightScale = this.data.height * 0.0254; // Convert height from inches to meters
-    this.el.object3D.scale.set(widthScale, heightScale, 1);
-  }
-});
+    const widthScale = this.data.width * 0.0254 // Convert width from inches to meters
+    const heightScale = this.data.height * 0.0254 // Convert height from inches to meters
+    this.el.object3D.scale.set(widthScale, heightScale, 1)
+  },
+})
 
 AFRAME.registerComponent("hexgrid", {
   schema: {
     id: { default: "" },
-    xlim : { default: 3 },
-    ylim : { default: 3 },
+    xlim: { default: 3 },
+    ylim: { default: 3 },
     widthInches: { default: 36 },
   },
 
   init: function () {
-    const d = this.data;
-    const id = d.id || this.el.id;
+    const d = this.data
+    const id = d.id || this.el.id
 
-    const patternId = config[id].patternId;
-    const tex1 = document.getElementById(patternId).src;
+    const patternId = config[id].patternId
+    const tex1 = document.getElementById(patternId).src
 
-    const hexData = config[id].data;
-    const hexes = hexData.hexes;
-    
+    const hexData = config[id].data
+    const hexes = hexData.hexes
+
     // Scale this object so that the width is exactly 36 inches
     // 1 inch = 0.0254 meters
     // Assume object scale is in meters
-    const pxWidth = hexData.w;
-    const scale = (d.widthInches * 0.0254) / pxWidth;
-    this.el.object3D.scale.set(scale, scale, scale);
+    const pxWidth = hexData.w
+    const scale = (d.widthInches * 0.0254) / pxWidth
+    this.el.object3D.scale.set(scale, scale, scale)
 
-    const w = hexData.w;
-    const h = hexData.h;
+    const w = hexData.w
+    const h = hexData.h
 
     // console.log(w, h);
 
@@ -121,18 +135,18 @@ AFRAME.registerComponent("hexgrid", {
       //   return isAnimableTile(col, row);
       // })
       .map(function (hex) {
-        const col = hex.c[0];
-        const row = hex.c[1];
-        return parseHexData(hex.c, hex.cax, hex.r, w, h);
-      });
+        const col = hex.c[0]
+        const row = hex.c[1]
+        return parseHexData(hex.c, hex.cax, hex.r, w, h)
+      })
 
-    this.attribInit = false;
-    this.initHexyMesh();
+    this.attribInit = false
+    this.initHexyMesh()
 
-    console.log("Total hexes: " + this.shapes.length);
+    console.log("Total hexes: " + this.shapes.length)
 
     // Create empty geometry, is modified every frame later
-    var geometry = new THREE.BufferGeometry();
+    var geometry = new THREE.BufferGeometry()
 
     // Initialize the material, it will be set when texture loads
     var material = new THREE.ShaderMaterial({
@@ -149,23 +163,23 @@ AFRAME.registerComponent("hexgrid", {
       transparent: true,
       depthWrite: false,
       blending: THREE.NormalBlending,
-    });
+    })
 
     // Create the mesh and set it to the entity
-    this.mesh = new THREE.Mesh(geometry, material);
-    this.el.setObject3D("mesh", this.mesh);
+    this.mesh = new THREE.Mesh(geometry, material)
+    this.el.setObject3D("mesh", this.mesh)
 
     // Texture loader with callbacks
-    var textureLoader = new THREE.TextureLoader();
+    var textureLoader = new THREE.TextureLoader()
     textureLoader.load(
       tex1,
       // onLoad callback
       function (texture) {
         // Apply the texture to the material
         // material.map = texture;
-        material.uniforms.tex.value = texture;
-        material.uniforms.grayToAlpha = { value: 0.0 };
-        material.needsUpdate = true;
+        material.uniforms.tex.value = texture
+        material.uniforms.grayToAlpha = { value: 0.0 }
+        material.needsUpdate = true
         // material.blending = THREE.AdditiveBlending;
         // material.blending = THREE.NORMALBLENDING;
         // material.transparent = true;
@@ -174,85 +188,92 @@ AFRAME.registerComponent("hexgrid", {
       undefined,
       // onError callback
       function (err) {
-        console.error("An error happened during texture loading.", err);
+        console.error("An error happened during texture loading.", err)
       }
-    );
+    )
 
     // Start animating
-    startAnim(9);
+    startAnim(9)
   },
 
   initHexyMesh: function () {
     // Initialize buffers with correct size
     // 7 verts/uvs per hex, 6 tris (18 indices) per hex
-    this.vertices = new Float32Array(7 * this.shapes.length * 3);
-    this.pivots = new Float32Array(7 * this.shapes.length * 2);
-    this.uvs = new Float32Array(7 * this.shapes.length * 2);
-    this.turns = new Float32Array(7 * this.shapes.length);
+    this.vertices = new Float32Array(7 * this.shapes.length * 3)
+    this.pivots = new Float32Array(7 * this.shapes.length * 2)
+    this.uvs = new Float32Array(7 * this.shapes.length * 2)
+    this.turns = new Float32Array(7 * this.shapes.length)
 
-    this.indices = [];
+    this.indices = []
 
     for (let i = 0; i < this.shapes.length; i++) {
-      const shape = this.shapes[i];
-      const indexOffset = i * 7; // current vertex count
+      const shape = this.shapes[i]
+      const indexOffset = i * 7 // current vertex count
 
       // Push verts as separate numbers in a single array
       for (let j = 0; j < shape.verts.length; j++) {
-        let v = shape.verts[j];
-        const vOffset = (i * 7 + j) * 3;
-        this.vertices[vOffset] = v.x;
-        this.vertices[vOffset + 1] = v.y;
-        this.vertices[vOffset + 2] = 0;
+        let v = shape.verts[j]
+        const vOffset = (i * 7 + j) * 3
+        this.vertices[vOffset] = v.x
+        this.vertices[vOffset + 1] = v.y
+        this.vertices[vOffset + 2] = 0
 
-        const pivotOffset = (i * 7 + j) * 2;
-        this.pivots[pivotOffset] = shape.pivot.x;
-        this.pivots[pivotOffset + 1] = shape.pivot.y;
+        const pivotOffset = (i * 7 + j) * 2
+        this.pivots[pivotOffset] = shape.pivot.x
+        this.pivots[pivotOffset + 1] = shape.pivot.y
 
-        const uvOffset = (i * 7 + j) * 2;
-        this.uvs[uvOffset] = shape.uvs[j].x;
-        this.uvs[uvOffset + 1] = shape.uvs[j].y;
+        const uvOffset = (i * 7 + j) * 2
+        this.uvs[uvOffset] = shape.uvs[j].x
+        this.uvs[uvOffset + 1] = shape.uvs[j].y
 
-        const turnOffset = i * 7 + j;
-        this.turns[turnOffset] = 0;
+        const turnOffset = i * 7 + j
+        this.turns[turnOffset] = 0
       }
 
       for (let j = 0; j < shape.tris.length; j++) {
-        this.indices[i * 18 + j] = shape.tris[j] + indexOffset;
+        this.indices[i * 18 + j] = shape.tris[j] + indexOffset
       }
     }
   },
 
   updateHexyMesh: function () {
-    stepAnimate();
+    stepAnimate()
 
-    const turnsAttrib = this.mesh.geometry.attributes.turns;
+    const turnsAttrib = this.mesh.geometry.attributes.turns
     if (!turnsAttrib) {
-      return;
+      return
     }
 
     for (let i = 0; i < this.shapes.length; i++) {
-      const shape = this.shapes[i];
+      const shape = this.shapes[i]
 
       // const offset = vertices.length;
-      const indexOffset = i * 7; // current vertex count
+      const indexOffset = i * 7 // current vertex count
 
       // Animation
-      let animCx, animCy;
-      let randN = getAnimCompleteCount(8);
-      animCx = Math.round(sqRand(randN) * 3 - 1);
-      animCy = Math.round(sqRand(randN + 1) * 2 - 1);
+      let animCx, animCy
+      let randN = getAnimCompleteCount(8)
+      animCx = Math.round(sqRand(randN) * 3 - 1)
+      animCy = Math.round(sqRand(randN + 1) * 2 - 1)
 
-      const dist = getDistOddr(shape.cOddr, vec2(animCx, animCy));
-      const animActive = isAnimActive(dist);
-      const animProgress = getAnimProgress(dist);
-      const animCompleteCount = getAnimCompleteCount(dist);
-      let animTurns = animCompleteCount + easeInOutQuad(animProgress);
+      const dist = getDistOddr(shape.cOddr, vec2(animCx, animCy))
+      const animActive = isAnimActive(dist)
+      const animProgress = getAnimProgress(dist)
+      const animCompleteCount = getAnimCompleteCount(dist)
+      let animTurns = animCompleteCount + easeInOutQuad(animProgress)
 
-      if (!isAnimableTile(shape.cOddr.x, shape.cOddr.y, this.data.xlim, this.data.ylim)) {
-        animTurns = 0;
+      if (
+        !isAnimableTile(
+          shape.cOddr.x,
+          shape.cOddr.y,
+          this.data.xlim,
+          this.data.ylim
+        )
+      ) {
+        animTurns = 0
       }
 
-      const turnOffsetPerShape = i * 7;
+      const turnOffsetPerShape = i * 7
       // Push verts as separate numbers in a single array
       for (let j = 0; j < shape.verts.length; j++) {
         // let v = shape.verts[j];
@@ -263,34 +284,46 @@ AFRAME.registerComponent("hexgrid", {
         // this.vertices[vOffset + 2] = 0;
 
         // this.turns[turnOffsetPerShape + j] = animTurns;
-        turnsAttrib.array[turnOffsetPerShape + j] = animTurns;
+        turnsAttrib.array[turnOffsetPerShape + j] = animTurns
       }
 
-      turnsAttrib.needsUpdate = true;
+      turnsAttrib.needsUpdate = true
     }
 
     // TODO use a dirty flag to only update when needed
   },
 
   tick: function (time, timeDelta) {
-    setMillis(time);
+    setMillis(time)
 
     // set time attribute
 
-    this.updateHexyMesh();
+    this.updateHexyMesh()
 
     if (!this.attribInit) {
-      this.mesh.geometry.setIndex(this.indices);
-      this.mesh.geometry.setAttribute("uv", new THREE.BufferAttribute(this.uvs, 2));
-      this.mesh.geometry.setAttribute("pivot", new THREE.BufferAttribute(this.pivots, 2));
-      this.mesh.geometry.setAttribute("turns", new THREE.BufferAttribute(this.turns, 1));
-      this.mesh.geometry.setAttribute("position", new THREE.BufferAttribute(this.vertices, 3));
-      this.attribInit = true;
+      this.mesh.geometry.setIndex(this.indices)
+      this.mesh.geometry.setAttribute(
+        "uv",
+        new THREE.BufferAttribute(this.uvs, 2)
+      )
+      this.mesh.geometry.setAttribute(
+        "pivot",
+        new THREE.BufferAttribute(this.pivots, 2)
+      )
+      this.mesh.geometry.setAttribute(
+        "turns",
+        new THREE.BufferAttribute(this.turns, 1)
+      )
+      this.mesh.geometry.setAttribute(
+        "position",
+        new THREE.BufferAttribute(this.vertices, 3)
+      )
+      this.attribInit = true
     } else {
-      const uniforms = this.mesh.material.uniforms;
-      uniforms.time.value = time;
-      uniforms.grayToAlpha.value = this.data.grayToAlpha ? 1.0 : 0.0;
-    
+      const uniforms = this.mesh.material.uniforms
+      uniforms.time.value = time
+      uniforms.grayToAlpha.value = this.data.grayToAlpha ? 1.0 : 0.0
+
       // Update the attributes without creating new BufferAttributes
       // this.mesh.geometry.attributes.position.set(this.vertices);
       // this.mesh.geometry.attributes.position.needsUpdate = true;
@@ -301,34 +334,34 @@ AFRAME.registerComponent("hexgrid", {
 
       // this.mesh.geometry.attributes.turns.set(this.turns);
       // this.mesh.geometry.attributes.turns.needsUpdate = true;
-    
+
       // Mark the attributes as needing an update
     }
 
     // Update the geometry to reflect the changes
-    this.mesh.geometry.computeVertexNormals();
-    this.mesh.geometry.computeBoundingBox();
-    this.mesh.geometry.computeBoundingSphere();
+    this.mesh.geometry.computeVertexNormals()
+    this.mesh.geometry.computeBoundingBox()
+    this.mesh.geometry.computeBoundingSphere()
   },
-});
+})
 
 function isAnimableTile(col, row, xlim, ylim) {
-  const isEvenRow = row % 2 === 0;
-  const xMin = isEvenRow ? -xlim : -xlim - 1;
-  const xMax = xlim;
-  const yMin = -ylim;
-  const yMax = ylim;
+  const isEvenRow = row % 2 === 0
+  const xMin = isEvenRow ? -xlim : -xlim - 1
+  const xMax = xlim
+  const yMin = -ylim
+  const yMax = ylim
   if (col < xMin || col > xMax || row < yMin || row > yMax) {
-    return false;
+    return false
   } else {
-    return true;
+    return true
   }
 }
 
 function parseHexData(centerOddrArr, centerAxArr, radius, texWidth, texHeight) {
   // Something that can be turned into a mesh with all necessary attributes
-  const centerOddrVec = vec2(centerOddrArr[0], centerOddrArr[1]);
-  const centerAxVec = vec2(centerAxArr[0], centerAxArr[1]);
+  const centerOddrVec = vec2(centerOddrArr[0], centerOddrArr[1])
+  const centerAxVec = vec2(centerAxArr[0], centerAxArr[1])
 
   const shape = {
     cOddr: centerOddrVec,
@@ -337,36 +370,36 @@ function parseHexData(centerOddrArr, centerAxArr, radius, texWidth, texHeight) {
     uvs: [],
     tris: [],
     pivot: vec2(0, 0), // cartesian
-  };
+  }
 
-  const centerCart = hexToCartesianOddr(centerOddrVec, radius);
+  const centerCart = hexToCartesianOddr(centerOddrVec, radius)
 
-  const poly = new Polygon(centerCart, radius, 6, Math.PI / 2); // pointy side up
-  const pts = poly.getPoints();
+  const poly = new Polygon(centerCart, radius, 6, Math.PI / 2) // pointy side up
+  const pts = poly.getPoints()
 
-  shape.pivot = centerCart;
+  shape.pivot = centerCart
 
-  shape.verts.push(...pts); // ring
-  shape.verts.push(centerCart); // center
-  const iCenter = pts.length;
+  shape.verts.push(...pts) // ring
+  shape.verts.push(centerCart) // center
+  const iCenter = pts.length
 
   // Normalize uvs, assume the texture is the full range from
   // -h/2 to h/2 and -w/2 to w/2
-  const uvs = pts.map((p) => cartToUv(p, texWidth, texHeight));
-  shape.uvs.push(...uvs);
-  shape.uvs.push(cartToUv(centerCart, texWidth, texHeight));
+  const uvs = pts.map((p) => cartToUv(p, texWidth, texHeight))
+  shape.uvs.push(...uvs)
+  shape.uvs.push(cartToUv(centerCart, texWidth, texHeight))
 
   // Triangulate
   for (let i = 0; i < pts.length; i++) {
-    const iNext = (i + 1) % pts.length;
+    const iNext = (i + 1) % pts.length
 
     // can't remember if this is CW or CCW, flip if needed
-    shape.tris.push(i, iNext, iCenter);
+    shape.tris.push(i, iNext, iCenter)
   }
 
-  return shape;
+  return shape
 }
 
 function cartToUv(p, texWidth, texHeight) {
-  return vec2(p.x / texWidth + 0.5, p.y / texHeight + 0.5);
+  return vec2(p.x / texWidth + 0.5, p.y / texHeight + 0.5)
 }
