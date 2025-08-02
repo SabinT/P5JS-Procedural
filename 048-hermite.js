5899cdb781e1e5230f0498b30aa502210369eff3
Sabin T <sabintim@outlook.com>
2025-08-02 02:11:50 -0700

    More hermite stuff

--- 048-hermite.js
+++ 048-hermite.js
@@ -6,7 +6,7 @@ const h = 800;
 
 let splines = [];
 let state = 0; // 0 = p0, 1 = m0, 2 = p1, 3 = m1
-let current = { p0: null, m0: null, p1: null, m1: null };
+let wipSpline = { p0: null, m0: null, p1: null, m1: null };
 let dragStart;
 let previewBuffer;
 
@@ -29,10 +29,10 @@ window.draw = function () {
   // Draw in-progress selections
   if (state > 0) {
     stroke(100, 200, 255);
-    if (current.p0) ellipse(current.p0.x, current.p0.y, 6);
-    if (current.m0) line2D(current.p0, add2d(current.p0, current.m0));
-    if (current.p1) ellipse(current.p1.x, current.p1.y, 6);
-    if (current.m1) line2D(current.p1, add2d(current.p1, current.m1));
+    if (wipSpline.p0) ellipse(wipSpline.p0.x, wipSpline.p0.y, 6);
+    if (wipSpline.m0) line2D(wipSpline.p0, add2d(wipSpline.p0, wipSpline.m0));
+    if (wipSpline.p1) ellipse(wipSpline.p1.x, wipSpline.p1.y, 6);
+    if (wipSpline.m1) line2D(wipSpline.p1, add2d(wipSpline.p1, wipSpline.m1));
   }
 
   // Draw picking previews
@@ -45,19 +45,22 @@ window.draw = function () {
 window.mousePressed = function () {
   dragStart = vec2(mouseX, mouseY);
 
-  if (state === 0) { current.p0 = dragStart; state = 1; }
-  else if (state === 2) { current.p1 = dragStart; state = 3; }
+  if (state === 0) { wipSpline.p0 = dragStart; state = 1; }
+  else if (state === 2) { wipSpline.p1 = dragStart; state = 3; }
 };
 
 window.mouseReleased = function () {
   if (state === 1) {
-    current.m0 = sub2d(vec2(mouseX, mouseY), current.p0);
+    wipSpline.m0 = sub2d(vec2(mouseX, mouseY), wipSpline.p0);
     state = 2;
   } else if (state === 3) {
-    current.m1 = sub2d(vec2(mouseX, mouseY), current.p1);
+    wipSpline.m1 = sub2d(vec2(mouseX, mouseY), wipSpline.p1);
 
-    splines.push(new CubicHermite(current.p0, current.m0, current.p1, current.m1));
-    current = { p0: null, m0: null, p1: null, m1: null };
+    const spline = new CubicHermite(wipSpline.p0, wipSpline.m0, wipSpline.p1, wipSpline.m1);
+    splines.push(spline);
+    console.log("Added spline:", spline.ToJSONString());
+
+    wipSpline = { p0: null, m0: null, p1: null, m1: null };
     state = 0;
   }
 };
@@ -81,7 +84,7 @@ function drawSpline(spline) {
     line2D(p, add2d(p, scale2d(tangent, debugLength)));
 
     // Normals
-    const normal = normalize2d(spline.GetBinormal(t));
+    const normal = normalize2d(spline.GetNormal(t));
     stroke("green");
     line2D(p, add2d(p, scale2d(normal, debugLength)));
   }
@@ -96,23 +99,23 @@ function drawPickingPreview() {
   if (state === 1) {
     // Realtime tangent preview for m0
     previewBuffer.stroke(100, 200, 255);
-    previewBuffer.line(current.p0.x, current.p0.y, mouseX, mouseY);
+    previewBuffer.line(wipSpline.p0.x, wipSpline.p0.y, mouseX, mouseY);
   }
 
   if (state === 3) {
     // Realtime tangent preview for m1
     previewBuffer.stroke(100, 200, 255);
-    previewBuffer.line(current.p1.x, current.p1.y, mouseX, mouseY);
+    previewBuffer.line(wipSpline.p1.x, wipSpline.p1.y, mouseX, mouseY);
   }
 
   if (state === 2 || state === 3) {
     // Show realtime spline preview (20 samples)
-    const p0 = current.p0;
-    const m0 = current.m0;
-    const p1 = (state === 2) ? vec2(mouseX, mouseY) : current.p1;
+    const p0 = wipSpline.p0;
+    const m0 = wipSpline.m0;
+    const p1 = (state === 2) ? vec2(mouseX, mouseY) : wipSpline.p1;
     const m1 = (state === 2)
       ? sub2d(vec2(mouseX, mouseY), p1)
-      : sub2d(vec2(mouseX, mouseY), current.p1);
+      : sub2d(vec2(mouseX, mouseY), wipSpline.p1);
 
     const tempSpline = new CubicHermite(p0, m0, p1, m1);
 
--- lumic/hermite.js
+++ lumic/hermite.js
@@ -1,4 +1,4 @@
-import { vec2, scale2d } from './common.js';
+import { vec2, scale2d, normalize2d } from './common.js';
 
 export class CubicHermite {
     constructor(p0,m0,p1,m1) {
@@ -32,10 +32,10 @@ export class CubicHermite {
         const h01 = -6 * t2 + 6 * t;
         const h11 = 3 * t2 - 2 * t;
 
-        return vec2(
+        return normalize2d(vec2(
             h00 * this.p0.x + h10 * this.m0.x + h01 * this.p1.x + h11 * this.m1.x,
             h00 * this.p0.y + h10 * this.m0.y + h01 * this.p1.y + h11 * this.m1.y
-        );
+        ));
     }
 
     GetNormal(t) {
@@ -45,15 +45,50 @@ export class CubicHermite {
         const h01 = -12 * t + 6;
         const h11 = 6 * t - 2;
 
-        return vec2(
+        return normalize2d(vec2(
             h00 * this.p0.x + h10 * this.m0.x + h01 * this.p1.x + h11 * this.m1.x,
             h00 * this.p0.y + h10 * this.m0.y + h01 * this.p1.y + h11 * this.m1.y
-        );
+        ));
     }
 
     GetBinormal(t) {
         // perpendicular to the tangent
         const tangent = this.GetTangent(t);
         return vec2(-tangent.y, tangent.x);
+    }
+
+    GetPoints(segments = 16) {
+        const points = [];
+        for (let i = 0; i <= segments; i++) {
+            const t = i / segments;
+            points.push(this.GetPosition(t));
+        }
+        return points;
+    }
+
+    Draw(segments = 16) {
+        const points = this.GetPoints(segments);
+        beginShape();
+        for (const point of points) {
+            vertex(point.x, point.y);
+        }
+        endShape();
+    }
+
+    ToJSONString() {
+        return JSON.stringify({
+            p0: { x: this.p0.x, y: this.p0.y },
+            m0: { x: this.m0.x, y: this.m0.y },
+            p1: { x: this.p1.x, y: this.p1.y },
+            m1: { x: this.m1.x, y: this.m1.y }
+        });
+    }
+
+    FromJSONString(jsonString) {
+        const data = JSON.parse(jsonString);
+        this.p0 = vec2(data.p0.x, data.p0.y);
+        this.m0 = vec2(data.m0.x, data.m0.y);
+        this.p1 = vec2(data.p1.x, data.p1.y);
+        this.m1 = vec2(data.m1.x, data.m1.y);
     }
 }
